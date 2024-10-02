85a1d9562dab2ff1ba76cd14883a3f17f874ae3e
Christopher Gallant <thraxus@gmail.com>
2024-10-02 14:09:04 -0300

    fix index in transpiler for GetExplanationUnfinalized

--- 1.5/Source/VSE/Properties/AssemblyInfo.cs
+++ 1.5/Source/VSE/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9041.30441")]
+[assembly: AssemblyVersion("1.0.9041.30659")]
 [assembly: AssemblyFileVersion("1.0.9041.28621")]
 
 [assembly: AssemblyInformationalVersion("1.0.2")]
